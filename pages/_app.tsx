1cc3800cccf8e5484ca14457eced252dfef19e26
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2021-08-31 19:58:41 +0430

    Remove a few dead codes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,6 @@ import { AppProps } from 'next/app';
 import Visitor from '../layouts/visitor/visitor';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // const getLayout = Component.getLayout || ((page) => page);
-  // return getLayout(<Component {...pageProps} />);
-
   return (
     <Visitor>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,3 @@
-// import { useRouter } from 'next/router';
-
 export default function Home() {
-  // const router = useRouter();
-
-  // router.push('/products');
-
   return null;
 }
